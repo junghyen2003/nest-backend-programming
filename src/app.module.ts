5fc757b40b2bccac12c46f441839851dd676f489
조중현 <junghyen2003@gmail.com>
2023-11-06 12:57:39 +0900

    하위 도메인 라우팅 처리

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { ApiController } from './api/api.controller';
 
 @Module({
-  controllers: [AppController, UsersController],
+  // 같은 엔드포인트 일 경우 인덱스에 따라 처리
+  controllers: [ApiController, AppController, UsersController],
   providers: [AppService, UsersService],
 })
 export class AppModule {}
