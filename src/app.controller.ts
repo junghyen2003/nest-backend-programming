7a72190107dd0896e1a50b52a71d376777ab92e2
조중현 <junghyen2003@gmail.com>
2023-11-13 15:23:44 +0900

    설정 파일 로드 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  @Get()
-  getHello(): string {
+  constructor(private readonly configService: ConfigService) {}
+  @Get('/db-host-from-config')
+  getDatabaseHostFromConfigService(): string {
+    return this.configService.get('');
     return process.env.DATABASE_HOST;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [
+    UsersModule,
+    EmailModule,
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.production.env'
+          : process.env.NODE_ENV === 'stage'
+          ? '.stage.env'
+          : '.development.env',
+    }), // forRoot : DynamicModule을 리턴하는 정적 메서드
+  ],
   controllers: [AppController],
   providers: [],
 })
