cc0bef3df779e12573c31f32b093cad6758c7de6
조중현 <junghyen2003@gmail.com>
2023-11-20 11:34:08 +0900

    커스텀 Config 파일 작성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,6 @@ export class AppController {
   constructor(private readonly configService: ConfigService) {}
   @Get('/db-host-from-config')
   getDatabaseHostFromConfigService(): string {
-    return this.configService.get('');
-    return process.env.DATABASE_HOST;
+    return this.configService.get('DATABASE_HOST');
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,20 @@ import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
 
 @Module({
   imports: [
     UsersModule,
     EmailModule,
     ConfigModule.forRoot({
-      envFilePath:
-        process.env.NODE_ENV === 'production'
-          ? '.production.env'
-          : process.env.NODE_ENV === 'stage'
-          ? '.stage.env'
-          : '.development.env',
-    }), // forRoot : DynamicModule을 리턴하는 정적 메서드
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
   ],
   controllers: [AppController],
   providers: [],
